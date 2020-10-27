1dac161763b7d998bb0a33eac42daabce815eb80
Jason Mobarak <jason@swiftnav.com>
2020-10-26 23:18:31 -0700

    move so we don't run the build multiple times

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -28,3 +28,26 @@ elif [[ "$TRAVIS_OS_NAME" == "osx" ]] || [[ "$TRAVIS_OS_NAME" == "windows" ]]; t
 
   cargo build --release
 fi
+
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+  cd target/release;
+  7z a -tzip ../../revproxy_windows.zip rev-proxy.exe;
+  cd ../..;
+  VERSION="$(git describe --always --tags)";
+  mv revproxy_windows.zip "revproxy-${VERSION}-windows-x86_64.zip";
+  ls -l;
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  tar -C "target/release" -czf revproxy_macos.tar.gz rev-proxy;
+  VERSION="$(git describe --always --tags)";
+  mv revproxy_macos.tar.gz "revproxy-${VERSION}-macos-x86_64.tar.gz";
+  ls -l;
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  tar -C "target/x86_64-unknown-linux-musl/release" -czf revproxy_linux.tar.gz rev-proxy;
+  VERSION="$(git describe --always --tags)";
+  mv revproxy_linux.tar.gz "revproxy-${VERSION}-linux-x86_64.tar.gz";
+  ls -l;
+fi
